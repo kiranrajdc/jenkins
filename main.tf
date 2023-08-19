3f37453687cb1346711c9b11265280ae48928822
kiranrajdc <113894188+kiranrajdc@users.noreply.github.com>
2023-08-19 07:48:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 provider "aws" {
     region = "us-east-1"
   access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  secret_key = var.aws_secret_access_key_id
 }
 
 resource "aws_instance" "EC2FROMTF" {
