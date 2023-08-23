b2f6785b424f1cfb0011410063128b93d65ecf8b
kiranrajdc <113894188+kiranrajdc@users.noreply.github.com>
2023-08-23 05:43:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,9 +22,4 @@ resource "aws_instance" "EC2FROMTF2" {
   ami = "ami-0149b2da6ceec4bb0"
   instance_type = "t2.micro"
 }
-resource "aws_instance" "EC2FROMTF3" {
-  #name="EC2FROMTF"
-  ami = "ami-0149b2da6ceec4bb0"
-  instance_type = "t2.micro"
-}
 
